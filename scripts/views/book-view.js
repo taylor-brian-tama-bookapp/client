a158a24e6d89f4e1a638570861912e074e943789
Taylor Stemple <taylorstemple@gmail.com>
2018-01-23 09:07:01 -0800

    database declaration

---
+++ scripts/views/book-view.js
@@ -0,0 +1,3 @@
+'use strict';
+
+var __API_URL__ = 'postgres://ufupbwasntcdow:2b91e2e8820a8f374e22283400b001b89103da1de642bc3d6821264e4d4087e9@ec2-54-163-237-249.compute-1.amazonaws.com:5432/ddohnecq4qookv';
