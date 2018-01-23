bddd6750f2573e4dd4f1df882c66504ae0784808
Taylor Stemple <taylorstemple@gmail.com>
2018-01-23 11:51:06 -0800

    changed naming conventions, added initpage function

--- scripts/views/book-view.js
+++ scripts/views/book-view.js
@@ -2,5 +2,11 @@
 
 // var __API_URL__ = 'postgres://ufupbwasntcdow:2b91e2e8820a8f374e22283400b001b89103da1de642bc3d6821264e4d4087e9@ec2-54-163-237-249.compute-1.amazonaws.com:5432/ddohnecq4qookv';
 
+let bookView = {};
 
+bookView.initIndexPage = function() {
+    
+    
+    Book.all.map(book => $('#books').append(book.toHtml()));
+}
 
