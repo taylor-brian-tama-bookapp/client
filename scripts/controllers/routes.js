e8c70f9563611755cdc905f2feab917a6d2b2c96
Taylor Stemple <taylorstemple@gmail.com>
2018-01-24 12:57:46 -0800

    refactored page routing

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -4,6 +4,6 @@ page('/client', function(){ page.redirect('/');});
 page('/', homeView.init, app.Book.fetchAll);
 page('/new', newBookView.init);
 page('/book/:book', singleBookView.init);
-page('*', errorView.init);
+// page('*', errorView.init);
 
 page();
