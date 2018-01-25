5e060cf2b743e39cf7b6b5da461bf9af353753a6
Brian Bixby <brianbixby0@gmail.com>
2018-01-24 17:31:38 -0800

    error page fixed on my local machine

--- scripts/controllers/routes.js
+++ scripts/controllers/routes.js
@@ -1,16 +1,20 @@
 'use strict';
 
-page('/', function(){ page.redirect('/client');});
-page('/client/', homeView.init, app.Book.fetchAll);
-page('/client/new', newBookView.init);
-page('/client/book/:book', singleBookView.init);
-page('/client/*', errorView.init);
+// page('/', function(){ page.redirect('/client');});
+// page('/client/', homeView.init, app.Book.fetchAll);
+// page('/client/new', newBookView.init);
+// page('/client/book/:book', singleBookView.init);
+// page('/client/*', errorView.init);
+// page('/client/*/*', errorView.init);
+// page('/client/*/*/*', errorView.init);
 
-// //page('/client', function(){ page.redirect('/');});
-// page('/', homeView.init, app.Book.fetchAll, app.Book.renderAll);
-// page('/new', newBookView.init);
-// page('/book/:book', singleBookView.init);
-// //page('/client/*', errorView.init);
+
+page('/', homeView.init, app.Book.fetchAll, app.Book.renderAll);
+page('/new', newBookView.init);
+page('/book/:book', singleBookView.init);
+page('/*', errorView.init);
+page('/*/*', errorView.init);
+page('/*/*/*', errorView.init);
 
 
 page();
